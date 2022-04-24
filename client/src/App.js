c097f68c65ed17877293e7753815916cf48ffd86
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-24 20:27:56 +0100

    initial setup. create react app, tailwind config, dependency install and basic component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+const App = () => {
+	return (
+		<div>
+			<h1>App</h1>
+			<Navbar />
+		</div>
+	);
+};
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			background: '#0a192f',
+			accent: '#f57dff',
+			primary: '#8892b0',
+			primaryLight: '#a8b2d1',
+			primaryDark: '#495670',
+			white: '#fff',
+		},
+		fontFamily: {
+			sans: ['Montserrat', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		},
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '9920px',
+			xl: '1200px',
+		},
+		extend: {},
+	},
+	plugins: [],
+};
