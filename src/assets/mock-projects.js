f49e980b527895b9c5d368361ea1186d4e367c7a
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-25 14:33:39 +0100

    mock up work page, mock data for projects

---
+++ src/assets/mock-projects.js
@@ -0,0 +1,103 @@
+const projectsData = [
+	{
+		id: 1,
+		title: 'Smokey and the Bandit',
+		body: 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.',
+		liveUrl: 'https://earthlink.net',
+		gitUrl: 'http://bluehost.com',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+		tag: 'frontend',
+	},
+	{
+		id: 2,
+		title: 'Scooby-Doo! Camp Scare',
+		body: 'Duis bibendum. Morbi non quam nec dui luctus rutrum.',
+		liveUrl: 'http://bizjournals.com',
+		gitUrl: 'https://prnewswire.com',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'frontend',
+	},
+	{
+		id: 3,
+		title: 'Men of Means',
+		body: 'Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
+		liveUrl: 'https://pinterest.com',
+		gitUrl: 'http://deliciousdays.com',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'full-stack',
+	},
+	{
+		id: 4,
+		title: "Mo' Money",
+		body: 'Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+		liveUrl: 'https://liveinternet.ru',
+		gitUrl: 'https://google.com.hk',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'backend',
+	},
+	{
+		id: 5,
+		title: 'Butterfly Girl',
+		body: 'Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+		liveUrl: 'http://sogou.com',
+		gitUrl: 'http://diigo.com',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'frontend',
+	},
+	{
+		id: 6,
+		title: 'My Daughter, the Socialist (I kori mou, i sosialistria)',
+		body: 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.',
+		liveUrl: 'https://unblog.fr',
+		gitUrl: 'https://usda.gov',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'frontend',
+	},
+	{
+		id: 7,
+		title: 'Assassination (Ansatsu) (Assassination, The) (Assassin, The)',
+		body: 'Nulla tellus. In sagittis dui vel nisl. Duis ac nibh.',
+		liveUrl: 'https://a8.net',
+		gitUrl: 'http://example.com',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'full-stack',
+	},
+	{
+		id: 8,
+		title: 'Game of Death',
+		body: 'Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
+		liveUrl: 'http://howstuffworks.com',
+		gitUrl: 'https://nih.gov',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'full-stack',
+	},
+	{
+		id: 9,
+		title: 'Deal of the Century',
+		body: 'Duis at velit eu est congue elementum. In hac habitasse platea dictumst.',
+		liveUrl: 'http://ftc.gov',
+		gitUrl: 'http://seattletimes.com',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'backend',
+	},
+	{
+		id: 10,
+		title: 'Haunting, The',
+		body: 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+		liveUrl: 'http://ehow.com',
+		gitUrl: 'http://mozilla.com',
+		imageUrl: 'http://dummyimage.com/113x100.png/5fa2dd/ffffff',
+
+		tag: 'backend',
+	},
+];
+
+export default projectsData;
