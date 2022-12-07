dabe2a3aa6d251bfc8f65831db355c1134f284e4
Adam Sackfield <adam@adamsackfield.uk>
2022-12-07 00:46:48 +0000

    Portfolio section, state set up working on styling

--- src/containers/LayoutContainer/LayoutContainer.jsx
+++ src/containers/LayoutContainer/LayoutContainer.jsx
@@ -2,12 +2,14 @@ import './LayoutContainer.scss';
 
 import Home from '../../components/Home/Home';
 import Skills from '../../components/Skills/Skills';
+import ProjectsContainer from '../ProjectsContainer/ProjectsContainer';
 
 const LayoutContainer = () => {
 	return (
 		<>
 			<Home />
 			<Skills />
+			<ProjectsContainer />
 		</>
 	);
 };
