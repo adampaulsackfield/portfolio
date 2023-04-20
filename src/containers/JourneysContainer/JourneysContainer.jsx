1fa26ae347c73eb807022db8b3a5cdb2215775c8
Adam Sackfield <adam@adamsackfield.uk>
2023-04-20 11:48:41 +0100

    remove unused imports

--- src/containers/JourneysContainer/JourneysContainer.jsx
+++ src/containers/JourneysContainer/JourneysContainer.jsx
@@ -7,42 +7,42 @@ import './JourneysContainer.scss';
 import Journey from '../../components/Journey/Journey';
 
 const JourneysContainer = () => {
-	const [roles, setRoles] = useState(journeys);
+  const [roles] = useState(journeys);
 
-	return (
-		<section className='journey'>
-			<h1 className='journey__header'>My Journey</h1>
+  return (
+    <section className='journey'>
+      <h1 className='journey__header'>My Journey</h1>
 
-			<div className='journey__text'>
-				<p>
-					As a technical specialist at Apple, I have developed strong
-					problem-solving and time management skills, as well as the ability to
-					work effectively with ambiguity. In addition to my experience at
-					Apple, I have also completed two boot-camps - one in software
-					engineering and another in cloud engineering - which have provided me
-					with a strong foundation in these areas.
-				</p>
-				<p>
-					I am confident in my ability to work with people, having given
-					one-to-many presentations and contributed to team projects. Overall,
-					my diverse skill set and experience make me a valuable asset in any
-					technical role.
-				</p>
-			</div>
+      <div className='journey__text'>
+        <p>
+          As a technical specialist at Apple, I have developed strong
+          problem-solving and time management skills, as well as the ability to
+          work effectively with ambiguity. In addition to my experience at
+          Apple, I have also completed two boot-camps - one in software
+          engineering and another in cloud engineering - which have provided me
+          with a strong foundation in these areas.
+        </p>
+        <p>
+          I am confident in my ability to work with people, having given
+          one-to-many presentations and contributed to team projects. Overall,
+          my diverse skill set and experience make me a valuable asset in any
+          technical role.
+        </p>
+      </div>
 
-			<article className='journey__image'>
-				<img src={profileImage} alt='' />
-			</article>
+      <article className='journey__image'>
+        <img src={profileImage} alt='' />
+      </article>
 
-			<article className='journey__experience'>
-				<h2 class='journey__header'>Experience</h2>
+      <article className='journey__experience'>
+        <h2 class='journey__header'>Experience</h2>
 
-				<article class='experience'>
-					{roles && roles.map((role) => <Journey role={role} />)}
-				</article>
-			</article>
-		</section>
-	);
+        <article class='experience'>
+          {roles && roles.map((role) => <Journey role={role} />)}
+        </article>
+      </article>
+    </section>
+  );
 };
 
 export default JourneysContainer;
--- src/containers/LayoutContainer/LayoutContainer.jsx
+++ src/containers/LayoutContainer/LayoutContainer.jsx
@@ -3,22 +3,21 @@ import './LayoutContainer.scss';
 import Home from '../../components/Home/Home';
 import Skills from '../../components/Skills/Skills';
 import ProjectsContainer from '../ProjectsContainer/ProjectsContainer';
-import Journey from '../../components/Journey/Journey';
 import Contact from '../../components/Contact/Contact';
 import Footer from '../../components/Footer/Footer';
 import JourneysContainer from '../JourneysContainer/JourneysContainer';
 
 const LayoutContainer = () => {
-	return (
-		<div className='container'>
-			<Home />
-			<Skills />
-			<ProjectsContainer />
-			<JourneysContainer />
-			<Contact />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className='container'>
+      <Home />
+      <Skills />
+      <ProjectsContainer />
+      <JourneysContainer />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default LayoutContainer;
